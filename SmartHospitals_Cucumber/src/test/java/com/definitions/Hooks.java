6c65adf65daefd34a02313ef8e4a9fd8d45ee383
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-04-10 14:47:36 +0530

    Login_functions_without_parallel_execution

--- SmartHospitals_Cucumber/src/test/java/com/definitions/Hooks.java
+++ SmartHospitals_Cucumber/src/test/java/com/definitions/Hooks.java
@@ -1,5 +1,27 @@
 package com.definitions;
 
-public class Hooks {
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
+import com.utils.HelperClass;
+
+public class Hooks {
+    @Before
+    public static void setUp() {
+        HelperClass.getDriver();
+    }
+
+    @After
+    public static void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            // Take screenshot if scenario fails
+            final byte[] screenshot = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+        HelperClass.quitDriver();
+    }
 }
