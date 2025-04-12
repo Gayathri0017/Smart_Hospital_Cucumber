01b8446068097084c351c1e4593d87ed87880fbc
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-04-13 00:24:53 +0530

    json report added

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
     features = "src\\test\\resources\\Features",
     glue = {"com.definitions", "com.utils"},
-    plugin = {"pretty", "html:target/cucumber-reports.html"},
+    plugin = {"pretty", "json:target/cucumber-reports.json"},
     monochrome = true,
     dryRun = false
 )
