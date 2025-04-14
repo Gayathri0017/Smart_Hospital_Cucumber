97b1d65ac114dadda8f0afd1a3355ddf3ef17169
Gayathri0017 <gayathri4866@gmail.com>
2025-04-14 18:47:00 +0530

    adding search feature

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -3,11 +3,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
-<<<<<<< HEAD
-    features = {"src\\test\\resources\\Features\\DoctorFeature.feature","src\\test\\resources\\Features\\SearchFeature.feature","src\\test\\resources\\Features\\PrescriptionFeature.feature"},
-=======
     features = {"src/test/resources/Features/AccountantFeature.feature"},
->>>>>>> branch 'main' of https://github.com/Gayathri0017/Smart_Hospital_Cucumber.git
     glue = {"com.definitions", "com.utils"},
     plugin = {"pretty", "html:target/cucumber-reports.html" , "json:target/cucumber-reports.json"},
     monochrome = true,
