fb6cd86e76157f48d78287b524517e38e03a2d99
Gayathri0017 <gayathri4866@gmail.com>
2025-04-17 10:38:14 +0530

    update hooks

--- SmartHospitals_Cucumber/src/test/java/com/definitions/Hooks.java
+++ SmartHospitals_Cucumber/src/test/java/com/definitions/Hooks.java
@@ -2,6 +2,11 @@ package com.definitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -13,11 +18,12 @@ public class Hooks{
         HelperClass.getDriver().navigate().refresh();
     }
     @After
-    public static void tearDown(Scenario scenario) {
+    public static void tearDown(Scenario scenario) throws IOException {
         if (scenario.isFailed()) {
             // Take screenshot if scenario fails
-            final byte[] screenshot = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
+        	File srcFile = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.FILE);
+        	File destFile = new File("screenshots/" + scenario.getName() + ".png");
+        	FileUtils.copyFile(srcFile, destFile);
         }
         HelperClass.quitDriver();
     }
