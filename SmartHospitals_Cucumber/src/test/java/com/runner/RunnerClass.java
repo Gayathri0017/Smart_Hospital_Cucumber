747bf270a717cf8b1d397032db60d04c560972ef
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-04-21 14:08:14 +0530

    new

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -3,8 +3,9 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
-    features = {"src\\test\\resources\\Features"},
+    features = {"src\\test\\resources\\Features\\AccountantFeatureExpenses.feature"},
     glue = {"com.definitions", "com.utils"},
+    tags="@ValidInput",
     plugin = {"pretty", "html:target/cucumber-reports.html" , "json:target/cucumber-reports.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",},
     monochrome = true,
     dryRun = false
