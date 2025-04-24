f3f8c328266f602bdaad27cd73b01fe18e318ea4
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-04-24 23:17:59 +0530

    patient payment definition altered

--- SmartHospitals_Cucumber/src/test/java/com/definitions/PatientPaymentDefinitions.java
+++ SmartHospitals_Cucumber/src/test/java/com/definitions/PatientPaymentDefinitions.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,6 +16,7 @@ import com.utils.HelperClass;
 import io.cucumber.java.en.Then;
 
 public class PatientPaymentDefinitions {
+	private static final Logger log = LogManager.getLogger(PatientPaymentDefinitions.class);
 	WebDriverWait wait = new WebDriverWait(HelperClass.getDriver(),Duration.ofSeconds(10));
 	PatientPaymentPageActions objPPPA = new PatientPaymentPageActions();  
 	@Then("the User clicks Ipd button")
@@ -23,21 +26,21 @@ public class PatientPaymentDefinitions {
 
 	@Then("the User clicks payment option")
 	public void the_user_clicks_payment_option() {
-		try {
+//		try {
 			objPPPA.ClickPayment();
 			objPPPA.ClickMakePayment();
 			System.out.println(objPPPA.getPaymentAmount());
 			objPPPA.ClickAddPaymentBtn();
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
+//		}catch(Exception e) {
+//			System.out.println(e.getMessage());
+//		}
 		
 	}
 	
 	@Then("the User provide the card details")
 	public void the_user_provide_the_card_details(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
 		objPPPA.switchFrame();
-		try {
+//		try {
 			
 		    List<Map<String , String>> cardDetails = dataTable.asMaps(String.class , String.class);
 		    for(Map<String , String> details:cardDetails) {
@@ -46,9 +49,9 @@ public class PatientPaymentDefinitions {
 		    	objPPPA.setCvv(details.get("CVV"));
 		    	objPPPA.setCountry(details.get("Country"));
 		    }
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+//		}catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
 	    
 	}
 	
@@ -65,23 +68,37 @@ public class PatientPaymentDefinitions {
 
 	@Then("the User can able to see the error message {string}")
 	public void the_user_can_able_to_see_the_error_message(String error) {
-		try {
 			if(error.contains("Your card number is invalid.")) {
-				System.out.println(objPPPA.getInvalidTxt());
-				Assert.assertTrue(objPPPA.getInvalidTxt().contains(error));
+				try {
+					System.out.println(objPPPA.getInvalidTxt());
+					Assert.assertTrue(objPPPA.getInvalidTxt().contains(error));
+					log.info("Assert gets passed in invalid card number ");
+				}catch (Exception e) {
+					log.error("Assert gets failed in invalid card number ");
+					throw e;
+				}
 			}
 			if(error.contains("Your card’s expiration year is in the past.")) {
-				System.out.println(objPPPA.getInvalidTxt());
-				Assert.assertTrue(objPPPA.getInvalidTxt().contains(error));
+				try {
+					System.out.println(objPPPA.getInvalidTxt());
+					Assert.assertTrue(objPPPA.getInvalidTxt().contains(error));
+					log.info("Assert gets passed in invalid card expiry date ");
+				}catch (Exception e) {
+					log.error("Assert gets failed in invalid card expiry date ");
+					throw e;
+				}
 			}
 			if(error.contains("Your card’s security code is incomplete.")) {
-				System.out.println(objPPPA.getInvalidTxt());
-				Assert.assertTrue(objPPPA.getInvalidTxt().contains(error));
+				try {
+					System.out.println(objPPPA.getInvalidTxt());
+					Assert.assertTrue(objPPPA.getInvalidTxt().contains(error));
+					log.info("Assert gets passed in invalid cvv number ");
+				}catch (Exception e) {
+					log.error("Assert gets failed in invalid cvv number ");
+					throw e;
+				}
 			}
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
+		
 		
 		
 	}
@@ -89,8 +106,15 @@ public class PatientPaymentDefinitions {
 
 @Then("the User can able to see the success message {string}")
 public void the_user_can_able_to_see_the_success_message(String msg) {
-	Assert.assertTrue(objPPPA.getPayementSuccessText().contains(msg));
-	System.out.println(objPPPA.getPayementSuccessText());
+	try {
+		Assert.assertTrue(objPPPA.getPayementSuccessText().contains(msg));
+		System.out.println(objPPPA.getPayementSuccessText());
+		log.info("Assert gets passed in succes for valid number ");
+	}catch (Exception e) {
+		log.error("Assert gets failed in succes for valid number ");
+		throw e;
+	}
+	
 }
 
 }
