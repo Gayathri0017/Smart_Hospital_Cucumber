037fc4d08945b830bd172613b7e5c4f97796e489
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-04-24 12:19:57 +0530

    Patient payment altered in runner class

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -4,7 +4,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 
-    features = {"src\\test\\resources\\Features\\PatientAppointment.feature"},
+    features = {"src\\test\\resources\\Features\\PatientPayment.feature"},
     glue = {"com.definitions", "com.utils"},
     plugin = {"pretty", "json:target/cucumber-reports/reports.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
     monochrome = true,
