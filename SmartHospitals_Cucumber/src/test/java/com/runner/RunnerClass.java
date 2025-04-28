9f47da3eca5ea4d42a908604b63219fce01b6fb7
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-04-28 21:47:24 +0530

    i have changed the runner file for patientappointment,patientdischarge,patientpayment,patientlogin feature

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -4,7 +4,10 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 
-    features = {"src\\test\\resources\\Features\\AccountantFeatureExpenses.feature","src\\test\\resources\\Features\\AccountantFeatureIncome.feature","src\\test\\resources\\Features\\LoginFeature.feature"},
+    features = {"src\\test\\resources\\Features\\PatientAppointment.feature",
+    		"src\\test\\resources\\Features\\PatientDischarge.feature",
+    		"src\\test\\resources\\Features\\PatientPayment.feature",
+    		"src\\test\\resources\\Features\\PatientLogin.feature"},
     glue = {"com.definitions", "com.utils"},
     plugin = {"pretty", "json:target/cucumber-reports/reports.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
     monochrome = true,
