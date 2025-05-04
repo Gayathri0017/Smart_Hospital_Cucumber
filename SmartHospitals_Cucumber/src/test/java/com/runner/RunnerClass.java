61ae9c0917f273b9325e2a7b442d82d27a4d1a1d
Gayathri0017 <gayathri4866@gmail.com>
2025-05-04 19:24:51 +0530

    Updated runner class

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -5,7 +5,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 
 
-   features = {"src\\test\\resources\\Features"},
+   features = {"src\\test\\resources\\Features\\SearchFeature.feature",
+		   "src\\test\\resources\\Features\\MessageFeature.feature",
+		   "src\\test\\resources\\Features\\PrescriptionFeature.feature",
+		   "src\\test\\resources\\Features\\DoctorFeature.feature"
+		   },
     glue = {"com.definitions", "com.utils"},
     plugin = {"pretty","json:target/cucumber-reports/reports.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
     monochrome = true,
