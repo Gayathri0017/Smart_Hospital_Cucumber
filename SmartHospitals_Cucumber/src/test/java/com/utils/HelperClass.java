1646a672025f0ab17e2d9ab8e22ca35123460ff6
Gayathri0017 <gayathri4866@gmail.com>
2025-05-05 13:57:59 +0530

    updated feature

--- SmartHospitals_Cucumber/src/test/java/com/utils/HelperClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/utils/HelperClass.java
@@ -1,5 +1,4 @@
 package com.utils;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,12 +8,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class HelperClass {
     private static ConcurrentHashMap<String, WebDriver> drivers = new ConcurrentHashMap<>();
-
     public static WebDriver getDriver() {
         String threadName = Thread.currentThread().getName();
         if (!drivers.containsKey(threadName)) {
             ChromeOptions options = new ChromeOptions();
-//            options.addArguments("--headless");
+            options.addArguments("--headless");
             options.addArguments("--incognito");
             options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
             options.setExperimentalOption("useAutomationExtension", false);
@@ -23,11 +21,12 @@ public class HelperClass {
                 put("profile.password_manager_enabled", false);
                 // Set the download directory
                 put("download.default_directory", "D:\\eclipse1\\Smart_Hospital_Cucumber\\SmartHospitals_Cucumber\\ReportDownlode");
-                put("download.prompt_for_download", false);
+                put("download.prompt_for_download", false); 
                 put("download.directory_upgrade", true);
                 put("safebrowsing.enabled", true);
             }});
 
+            
             WebDriver driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.manage().window().maximize();
