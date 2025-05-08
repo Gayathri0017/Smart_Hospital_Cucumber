5706debbb6ff674ac742eae935628d50bf2fc7c9
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-05-08 11:38:42 +0530

    changed as per the sonarqube issues

--- SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
+++ SmartHospitals_Cucumber/src/test/java/com/runner/RunnerClass.java
@@ -5,6 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 
    features = {"src\\test\\resources\\Features"},
+   
     glue = {"com.definitions", "com.utils"},
     plugin = {"pretty","json:target/cucumber-reports/reports.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
     monochrome = true,
